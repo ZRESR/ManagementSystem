2effb30f7b5c5b36df6d6df4069aa0a2356f1abd
estherrrrr <852212494@qq.com>
2017-11-26 21:06:30 +0800

    Initialize

---
+++ ManageSystem/Controllers/ClassController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: Class
+        public ActionResult ClassManage()
+        {
+            return View();
+        }
+        public ActionResult FixedRollStartCallUI()
+        {
+            return View();
+        }
+        public ActionResult GroupInfoUI2()
+        {
+            return View();
+        }
+        public ActionResult RollCallListUI()
+        {
+            return View();
+        }
+        public ActionResult StudentRollCallUI()
+        {
+            return View();
+        }
+    }
+}
