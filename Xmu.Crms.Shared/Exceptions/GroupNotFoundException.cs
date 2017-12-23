dcec8f263d7ce6f94b82682e88a09437f6f00f1a
lucity <34018426+lucity@users.noreply.github.com>
2017-12-23 12:41:34 +0800

    update Exception
    更新了EXCEPTION接口

--- Xmu.Crms.Shared/Exceptions/GroupNotFoundException.cs
+++ Xmu.Crms.Shared/Exceptions/GroupNotFoundException.cs
@@ -1,6 +1,33 @@
-﻿namespace Xmu.Crms.Shared.Exceptions
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Xmu.Crms.Shared.Exceptions
 {
-    public class GroupNotFoundException : System.Exception
+    [Serializable]
+    public class GroupNotFoundException : ArgumentOutOfRangeException
     {
+        public GroupNotFoundException()
+        {
+        }
+
+        public GroupNotFoundException(string paramName) : base(paramName)
+        {
+        }
+
+        public GroupNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public GroupNotFoundException(string paramName, string message) : base(paramName, message)
+        {
+        }
+
+        public GroupNotFoundException(string paramName, object actualValue, string message) : base(paramName, actualValue, message)
+        {
+        }
+
+        protected GroupNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- Xmu.Crms.Shared/Exceptions/TopicNotFoundException.cs
+++ Xmu.Crms.Shared/Exceptions/TopicNotFoundException.cs
@@ -1,6 +1,33 @@
-﻿namespace Xmu.Crms.Shared.Exceptions
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Xmu.Crms.Shared.Exceptions
 {
-    public class TopicNotFoundException : System.Exception
+    [Serializable]
+    public class TopicNotFoundException : ArgumentOutOfRangeException
     {
+        public TopicNotFoundException()
+        {
+        }
+
+        public TopicNotFoundException(string paramName) : base(paramName)
+        {
+        }
+
+        public TopicNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public TopicNotFoundException(string paramName, string message) : base(paramName, message)
+        {
+        }
+
+        public TopicNotFoundException(string paramName, object actualValue, string message) : base(paramName, actualValue, message)
+        {
+        }
+
+        protected TopicNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
